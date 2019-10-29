168299ac6c99c7b2715ac9125eaf9ff2ebe15fae
Q Wang <wangq@jfrog.com>
2019-10-29 19:09:04 +0800

    change text

--- account-service/src/main/java/org/wangqing/microservices/account/api/Api.java
+++ account-service/src/main/java/org/wangqing/microservices/account/api/Api.java
@@ -22,18 +22,12 @@ public class Api {
 	
 	public Api() {
 		accounts = new ArrayList<>();
-		accounts.add(new Account(1, 1, "111111"));
-		accounts.add(new Account(2, 2, "222222"));
-		accounts.add(new Account(3, 3, "333333"));
-		accounts.add(new Account(4, 4, "444444"));
-		accounts.add(new Account(5, 1, "555555"));
-		accounts.add(new Account(6, 2, "666666"));
-		accounts.add(new Account(7, 2, "777777"));
+		accounts.add(new Account(1, 1, "Helloworld"));
 	}
 	
 	@RequestMapping("/")
 	public String findByNumber(String number) {
-		serviceIsReady = "Account service is ready!";
+		serviceIsReady = "Hi JFrog webinar!";
 		return serviceIsReady;
 	}
 	
