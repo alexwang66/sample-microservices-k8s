fbe532d75cc3cc95fd534a5f7492aa35cd16c568
Q Wang <wangq@jfrog.com>
2020-01-27 17:11:34 +0800

    fix k8s deploy

--- account-service/src/main/java/org/wangqing/microservices/account/Application.java
+++ account-service/src/main/java/org/wangqing/microservices/account/Application.java
@@ -3,9 +3,11 @@ package org.wangqing.microservices.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 public class Application {
 
 	public static void main(String[] args) {
